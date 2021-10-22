f25c1b843da92d9f854c98c767649cfb1e20bc04
wrharrison23 <wrharrison23@gmail.com>
2021-10-22 13:29:38 -0400

    zendesk script

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+
 import Dashboard from './components/dashboard/dashboard.js';
 import accountList from './containers/accounts/accountsContainer.js';
 import FinalContacts from './containers/contacts/contactsContainer.js';
 import FinalOpportunities from './containers/opportunities/opportunitiesContainer.js';
 import FinalDetails from './containers/details/detailsContainer.js';
-
 
 // Routing done here
 const Main = () => (
