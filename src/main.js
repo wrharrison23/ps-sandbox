0eb47dcaeee354b83516c5127c40378b64a4698e
wrharrison23 <wrharrison23@gmail.com>
2021-10-22 13:32:45 -0400

    zendesk script take 5343

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/dashboard.js';
 import accountList from './containers/accounts/accountsContainer.js';
 import FinalContacts from './containers/contacts/contactsContainer.js';
@@ -9,11 +10,11 @@ import FinalDetails from './containers/details/detailsContainer.js';
 const Main = () => (
     <main>
         <Switch>
-            <Route exact path ='/' component={Dashboard}/>
-            <Route exact path='/accounts' component={accountList} />
-            <Route exact path='/contacts' component={FinalContacts} />
-            <Route exact path='/opportunities' component={FinalOpportunities} />
-            <Route path='/*/*/details' component={FinalDetails} />
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/accounts" component={accountList} />
+            <Route exact path="/contacts" component={FinalContacts} />
+            <Route exact path="/opportunities" component={FinalOpportunities} />
+            <Route path="/*/*/details" component={FinalDetails} />
         </Switch>
     </main>
 );
